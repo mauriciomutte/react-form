9f7d907380ef76b8f02ed24e1dc5547a889ac129
mauriciomutte <litiobr@gmail.com>
2020-01-28 14:30:32 -0300

    :sparkles: Add ApolloClient

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
+
+const Index = () => (
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>
+);
+
+render(<Index />, document.getElementById('root'));
